d350a3ccbc0932ee9f09278e5e5ce641b03361f9
alexkand1973 <KandaurovAV@mail.ru>
2022-12-04 18:20:23 +0300

    first commit

---
+++ app/src/main/java/com/alexkand/homeworkkotlin_12/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.alexkand.homeworkkotlin_12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    //Сделайте приложение в котором будут четыре activity.
+    // Главный экран и три других экрана. На главном экране будут три кнопки.
+    // С надписями: First activity, second activity, third activity,
+    // которые соответственно переносят на соответствующие экраны.
+    // На каждом экране, кроме главного экрана, будут TextView с текстом соответствующим данному экрану
+    // (например: First activity) и кнопка с надписью "Home",
+    // которая переносит на главный экран. В дизайне приложения желательно использовать LinearLayout.
+
+
+    }
+}
